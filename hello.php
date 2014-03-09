529f8c511940aa3987c01202c4b316e7315f2818
zmuda <ferry.2>
2014-03-09 13:32:53 +0100

    upload example

--- hello.php
+++ hello.php
@@ -1,8 +1,12 @@
 <html>
-<head>
-    <title>PHP Test</title>
-</head>
 <body>
-<?php echo '<p>Hello World</p>';?>
+
+<form action="upload_file.php" method="post"
+      enctype="multipart/form-data">
+    <label for="file">Filename:</label>
+    <input type="file" name="file" id="file"><br>
+    <input type="submit" name="submit" value="Submit">
+</form>
+
 </body>
 </html>
